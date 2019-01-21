811b8eca56751f51bba1f724f62ef3c5751aeae3
Eduard Ramirez <me@tantaroth.com>
2019-01-21 09:48:48 -0500

    Setup firebase credentials

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDTjBUcdNnvlL0dY084sRAxr2mh-Hv8MF8",
+    authDomain: "festivities-72ebe.firebaseapp.com",
+    databaseURL: "https://festivities-72ebe.firebaseio.com",
+    projectId: "festivities-72ebe",
+    storageBucket: "festivities-72ebe.appspot.com",
+    messagingSenderId: "817097069457"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDTjBUcdNnvlL0dY084sRAxr2mh-Hv8MF8",
+    authDomain: "festivities-72ebe.firebaseapp.com",
+    databaseURL: "https://festivities-72ebe.firebaseio.com",
+    projectId: "festivities-72ebe",
+    storageBucket: "festivities-72ebe.appspot.com",
+    messagingSenderId: "817097069457"
+  }
 };
 
 /*
