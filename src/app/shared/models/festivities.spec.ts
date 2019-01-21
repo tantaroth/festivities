2ea1b04eba53bfc545db9633bec2b68b60007bff
Eduard Ramirez <me@tantaroth.com>
2019-01-21 10:10:08 -0500

    Created models
    - User
    - Festivities

---
+++ src/app/shared/models/festivities.spec.ts
@@ -0,0 +1,3 @@
+import { TestBed, async } from '@angular/core/testing';
+import { Festivities } from "./festivities";
+
---
+++ src/app/shared/models/festivities.ts
@@ -0,0 +1,11 @@
+export interface Festivities {
+    name: string;
+    place: string;
+    startDate: Date;
+    endDate: Date;
+}
+export class Festivities {
+    constructor() {
+
+    }
+}
